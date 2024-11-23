9fce9b60765184231973b5b0ac8faa79c21c7190
Ondrejoda <ondrejoda@gmail.com>
2024-11-23 11:49:15 +0100

    mepoww

---
+++ GamebookPilar.Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GamebookPilar.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamebookPilar.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+            
+        }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
---
+++ GamebookPilar.Server/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace GamebookPilar.Server.Models
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+    }
+}
