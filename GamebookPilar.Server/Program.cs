804bb68b9adb2f03c659f00719ee38cc0b594f76
ondrejod <ondrejoda@gmail.com>
2024-11-25 20:43:53 +0100

    figured out the frontend backend connection (i hate cors with every fibre of my being)

--- GamebookPilar.Server/Program.cs
+++ GamebookPilar.Server/Program.cs
@@ -7,12 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.SetIsOriginAllowed(origin => new Uri(origin).IsLoopback);
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyMethod().SetIsOriginAllowed(origin => new Uri(origin).IsLoopback));
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
