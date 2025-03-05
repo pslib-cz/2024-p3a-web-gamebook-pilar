3580f948feb64ed9ddd0017fdb6fa1276edd9e34
ondrejod <ondrejoda@gmail.com>
2025-03-05 16:30:09 +0100

    ten kod je hotovej, jeste mi zbyva level design

--- GamebookPilar.Server/Models/Cutscene.cs
+++
@@ -1,9 +0,0 @@
-namespace GamebookPilar.Server.Models;
-
-public class Cutscene
-{
-    public int CutsceneId { get; set; }
-    public int TargetLocationId { get; set; }
-    
-    public ICollection<Frame> Frames { get; set; }
-}
